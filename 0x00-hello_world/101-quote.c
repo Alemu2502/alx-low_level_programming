ceb38356f19dbb4220fe4d90296ef829c18166a1
Alemu <fre6819@gmail.com>
2022-12-08 08:29:53 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *Return: Always 1
+ *Return:1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
