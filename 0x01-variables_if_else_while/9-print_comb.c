d9428ce12903c258ba97e3d525696f17cb6c8100
Alemu <fre6819@gmail.com>
2022-12-09 12:33:09 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Start of program execution
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	/*
+	 * loop for numbers 0-9
+	 * if number is 0-9, print number
+	 * then if number is 0-8, add comma plus space
+	 * else if number is 9, add only space
+	 */
+ for (num = '0'; num <= '9'; num++)
+ if (num >= '0')
+ {
+	       putchar(num);
+if (num >= '0' && num <= '8')
+{
+putchar(',');
+putchar(' ');
+}
+else
+putchar('\n');
+}
+return (0);
+}
