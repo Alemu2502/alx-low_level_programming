30d5d0801248039d47f3f94ad9453635d8f94846
Alemu <fre6819@gmail.com>
2022-12-09 11:51:39 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Start of program
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
