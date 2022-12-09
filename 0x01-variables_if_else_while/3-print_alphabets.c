4046de62257dcc8f0c9606e4c61d968a0a622771
Alemu <fre6819@gmail.com>
2022-12-09 11:26:06 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char alpha
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
