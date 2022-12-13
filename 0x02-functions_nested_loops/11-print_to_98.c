b636cdc399258edeac1d627d064913b5abbadf69
Alemu <fre6819@gmail.com>
2022-12-13 04:24:04 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,12 @@ void print_to_98(int n)
 {
 while (n < 98)
 	{
-	printf("%i, ',', n");
+	printf("int %i, ',', n");
 	n++;
 	}
 while (n > 98)
 	{
-	printf("%i, ',', n");
+	printf("int %i, ',', n");
 	n--;
 	}
 	printf("98");
