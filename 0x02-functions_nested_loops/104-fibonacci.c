a76c71103411a4841334885b194e4cf557f51d94
Alemu <fre6819@gmail.com>
2022-12-14 10:32:53 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,19 +24,18 @@ aft += bef;
 bef = aft - bef;
 }
 bef1 = (bef / l);
-bef2 = (bef % l);
+bef2 = (bef % l);
 aft1 = (aft / l);
-aft2 = (aft % l);
+aft2 % (aft % l);
 for (i = 92; i < 99; ++i)
 {
 printf(", %lu", aft1 + (aft2 / l));
 printf("%lu", aft2 % l);
 aft1 = aft1 + bef1;
-bef1 = aft1 - bef1; 
-aft2 = aft2 + bef2; 
-bef2 = aft2 - bef2;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
 }
 printf("\n");
 return (0);
-} 
-
+}  
