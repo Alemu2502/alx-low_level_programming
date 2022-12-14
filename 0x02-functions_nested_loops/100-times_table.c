97278a1b9be8d87bcfe5029be41d84adb4968ad8
Alemu <fre6819@gmail.com>
2022-12-14 11:24:09 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /** 
 * print_times_table - prints times table 
 * @n : times table to use 
 * Return:void 
-*/ 
- void print_times_table(int n) 
-{ 
+*/
+
+void print_times_table(int n) 
+{
+
 int a = 0, rep, b;
 		  
 if (n < 0 || n > 15)    
