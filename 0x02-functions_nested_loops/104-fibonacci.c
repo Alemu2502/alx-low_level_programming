f1fdda460b387cab6e7ae610ade1871e4b12aa27
Alemu <fre6819@gmail.com>
2022-12-14 10:50:08 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 unsigned long int i;
-unsigned long int bef = 1;
-unsigned long int aft = 2;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
 unsigned long int l = 1000000000;
 unsigned long int bef1;
 unsigned long int bef2;
