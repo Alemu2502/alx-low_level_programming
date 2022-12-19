d9576cd7c1f3724089f0143d381c320e43f9c11f
Alemu <fre6819@gmail.com>
2022-12-19 06:45:39 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by sring
+ * @dest: char type string
+ * @src: char type string
+ * Description: cope the string pointed to by pointer 'src' to 
+ * the buffer pointed to by 'dest'
+ * Return: pointer to 'dest'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+	retun(dest);
+}
