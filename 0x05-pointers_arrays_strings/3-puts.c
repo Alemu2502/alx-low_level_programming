db2de6f97882998ceb9449ce4390b62ce56e6913
Alemu <fre6819@gmail.com>
2022-12-19 05:16:30 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line
+ * Description: 3-puts.c
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
