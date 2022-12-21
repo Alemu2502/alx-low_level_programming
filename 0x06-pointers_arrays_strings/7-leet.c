89719afbb477fc9ebc11499b2d439e2732ba69ad
Alemu <fre6819@gmail.com>
2022-12-21 12:09:51 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet - transformed to leet
+ * @s: char array string type
+ * Return: s transformed
+ */
+
+char *leet(char *s)
+{
+int i, ii;
+	 char s1[] = "aeotl";
+	char s1[] = "AEOTL";
+	char s2[] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; ii < 5; ii++)
+		{
+			if (s[i] == s1[ii] || s[i] == s1[ii])
+			{
+				s[i] = s2[ii];
+				break;
+			}
+		}
+	}
+	return (s);
+}
