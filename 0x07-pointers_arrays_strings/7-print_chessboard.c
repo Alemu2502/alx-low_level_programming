77caf4bd5752dd6d26a1ab712feabd5fbae0c136
Alemu <fre6819@gmail.com>
2023-01-02 11:35:05 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chessboard.
+ * print_chessboard - prints chessboard
  * @a: matrix
+ * Return: void
  */
 
 void print_chessboard(char (*a)[8])
