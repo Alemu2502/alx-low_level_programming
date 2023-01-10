647647e9d92b37dae93c88cd0a0463106245eb4c
Alemu <fre6819@gmail.com>
2023-01-10 11:43:29 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,14 +8,14 @@
 
 int _strlen(char *s)
 {
-	int i = 0;
+	int l = 0;
 
 	while (*s != '\0')
 	{
 		s++;
-		i++;
+		l++;
 	}
-	return (i);
+	return (l);
 }
 /**
  * *str_concat - concat 2 strings
@@ -26,18 +26,18 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i1, i2;
+	unsigned int l1, l2;
 	char *conc, *tmp;
 
 	if (!s1)
 		s1 = "";
 	else
-		i1 = _strlen(s1);
+		l1 = _strlen(s1);
 	if (!s2)
 		s2 = "";
 	else
-		i2 = _strlen(s2);
-	conc = malloc(i1 + i2 + 1);
+		l2 = _strlen(s2);
+	conc = malloc(l1 + l2 + 1);
 	if (!conc)
 		return (0);
 	tmp = conc;
