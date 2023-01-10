f105c560fd8716c9a8c8313ef1b058c840c0cc23
Alemu <fre6819@gmail.com>
2023-01-10 11:19:46 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - copy
+ * @str: poiter to string
+ * Return: ponter
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, j;
+
+	if (!str)
+		return (NULL);
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+return (s);
+}
