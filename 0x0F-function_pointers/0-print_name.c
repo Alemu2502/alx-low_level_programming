5bba6ae52a3d555a8202a4240c93a8c3fe54db54
Alemu <fre6819@gmail.com>
2023-01-18 09:23:54 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_printers.h"
+
+/**
+ * print_name - prints name
+ * @name: name's main
+ * @f: void function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
