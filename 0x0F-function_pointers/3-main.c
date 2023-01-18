6426fb4628f5131b980b3fcdf2205a093103016b
Alemu <fre6819@gmail.com>
2023-01-18 10:54:00 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,8 +18,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	R = (*get_op_func(argv[2])) (atoi(argv[1]), atoi(argv[3]));
+
+	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", R);
+
 	return (0);
 }
-
