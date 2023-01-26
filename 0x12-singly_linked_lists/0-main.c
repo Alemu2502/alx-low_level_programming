940734783aaf117a66394c4f149b46c42fad79a3
Alemu <fre6819@gmail.com>
2023-01-26 10:30:15 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -21,7 +21,8 @@ if (new == NULL)
 printf("Error\n");
 return (1);
 }
-new->str = strdup("Hello");new->len = 5;
+new->str = strdup("Hello");
+new->len = 5;
 new->next = head;
 head = new;
 n = print_list(head);
